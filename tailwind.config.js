3c197b9fe6192fe06ca16b60b9adc8843d20c374
vitor <vitorpfiffer@gmail.com>
2024-06-26 22:22:47 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'f-black': "#121212",
+        'd-pink': "#820054",
+        'c-black': "#1e1e1e",
+        'l-gray': "#D9D9D9",
+        'll-gray': "#939393"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
